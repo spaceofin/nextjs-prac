7dee787784e82f7cb971a2e3e60db30efd0b9dec
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-08-18 21:31:43 +0900

    [nextjs-ts-practice]feat: add BlogPage for data fetching and display

---
+++ nextjs-ts-practice/app/blog/page.tsx
@@ -0,0 +1,29 @@
+interface Post {
+  id: number;
+  title: string;
+  author: string;
+}
+
+export default async function BlogPage() {
+  //   const posts = await fetch("http://localhost:3001/posts").then((res) =>
+  //     res.json()
+  //   );
+
+  const response = await fetch("http://localhost:3001/posts");
+  const posts = await response.json();
+
+  return (
+    <div className="flex flex-col">
+      <div className="mb-8 text-xl text-white">Posts</div>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id} className="mb-3 text-s">
+            <div>id: {post.id}</div>
+            <div>title: {post.title}</div>
+            <div>author: {post.author}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
