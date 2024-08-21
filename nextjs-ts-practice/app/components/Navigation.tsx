1dcc7e2456511257d35633ef2ba7060c05536272
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-08-21 19:52:01 +0900

    [nextjs-ts-practice]feat: apply responsive design to Navigation and Home components

--- nextjs-ts-practice/app/components/Navigation.tsx
+++ nextjs-ts-practice/app/components/Navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <nav className="font-mono">
-      <ul className="relative w-screen min-h-32 m-10 flex flex-col justify-center items-center text-2xl z-0">
+      <ul className="relative w-screen min-h-32 m-10 flex flex-col lg:flex-row lg:space-x-4 justify-center items-center text-2xl z-0">
         <li>
           <Link href="/">Home</Link>
         </li>
--- nextjs-ts-practice/app/page.tsx
+++ nextjs-ts-practice/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <span className="z-10">Hello,</span>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:z-[-1] after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert w-32 md:w-48 lg:w-64 xl:w-80 2xl:x-96"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
