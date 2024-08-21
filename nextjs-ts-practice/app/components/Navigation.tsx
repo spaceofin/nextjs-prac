2e946c8c5d238b5a7a9ed19f8df4a3f964b60020
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-08-22 01:02:44 +0900

    [nextjs-ts-practice]style: add Navigation.module.css and apply styles to Navigation component

--- nextjs-ts-practice/app/components/Navigation.tsx
+++ nextjs-ts-practice/app/components/Navigation.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
+import styles from "./Navigation.module.css";
+
 export default function Navigation() {
   return (
     <nav className="font-mono">
-      <ul className="relative w-screen min-h-32 m-10 flex flex-col lg:flex-row lg:space-x-4 justify-center items-center text-2xl z-0">
+      <ul className="relative w-screen min-h-32 m-10 flex flex-col lg:flex-row lg:space-x-4 space-y-1 justify-center items-center text-2xl lg:text-3xl z-0">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" className={styles.link}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href="/about" className={styles.link}>
+            About
+          </Link>
         </li>
         <li>
-          <Link href="/blog">Blog</Link>
+          <Link href="/blog" className={styles.link}>
+            Blog
+          </Link>
         </li>
       </ul>
     </nav>
