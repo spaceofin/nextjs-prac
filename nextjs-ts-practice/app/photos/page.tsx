a5ce31ba122803fb26c53eb551413d600bc35e2e
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-08-26 19:15:58 +0900

    [nextjs-ts-practice]refactor: replace <img> tags with Next.js Image component for optimization

--- nextjs-ts-practice/app/photos/page.tsx
+++ nextjs-ts-practice/app/photos/page.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+import tree from "../../public/images/tree.png";
+import flowers from "../../public/images/flowers.png";
+import road from "../../public/images/road.png";
+
 export default function PhotosPage() {
   return (
     <div>
       <h1 className="m-2 font-extralight text-3xl">PHOTOS PAGE</h1>
       <div className="grid grid-cols-3 gap-2">
-        <img src="/images/tree.png" />
-        <img src="/images/flowers.png" />
-        <img src="/images/road.png" />
+        <Image src={tree} alt="Picture of the tree" />
+        <Image src={flowers} alt="Picture of the flowers" />
+        <Image src={road} alt="Picture of the road" />
       </div>
     </div>
   );
