5aad1c68409929b791b6721c3648811e3dd88ee8
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-08-27 17:23:28 +0900

    [nextjs-ts-practice]feat: update Image components with sizes attribute for responsive behavior

--- nextjs-ts-practice/app/photos/page.tsx
+++ nextjs-ts-practice/app/photos/page.tsx
@@ -7,10 +7,22 @@ export default function PhotosPage() {
   return (
     <div>
       <h1 className="m-2 font-extralight text-3xl">PHOTOS PAGE</h1>
-      <div className="grid grid-cols-3 gap-2">
-        <Image src={tree} alt="Picture of the tree" />
-        <Image src={flowers} alt="Picture of the flowers" />
-        <Image src={road} alt="Picture of the road" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <Image
+          src={tree}
+          alt="Picture of the tree"
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+        />
+        <Image
+          src={flowers}
+          alt="Picture of the flowers"
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+        />
+        <Image
+          src={road}
+          alt="Picture of the road"
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+        />
       </div>
     </div>
   );
