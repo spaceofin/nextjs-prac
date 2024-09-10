41ce0aa387c27a7a19a24dd8d9b8b1dd40297707
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-09-10 18:19:32 +0900

    [nextjs-ts-practice]refactor: update toggleTheme function in useToggleTheme hook

--- nextjs-ts-practice/hooks/use-toggle-theme.tsx
+++ nextjs-ts-practice/hooks/use-toggle-theme.tsx
@@ -8,9 +8,16 @@ export default function useToggleTheme(defaultTheme: Theme = "light") {
   const toggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
-    // // document.documentElement.className = newTheme;
-    document.documentElement.classList.remove("light", "dark");
-    document.documentElement.classList.add(newTheme);
+
+    // document.documentElement.classList.remove("light", "dark");
+    // document.documentElement.classList.add(newTheme);
+
+    // document.documentElement.classList.toggle("light", theme === "dark");
+    // document.documentElement.classList.toggle("dark", theme === "light");
+
+    // document.documentElement.className = newTheme;
+
+    document.documentElement.classList.replace(theme, newTheme);
   };
 
   return { theme, toggleTheme };
