e1581e7c85aefd18237a2b8926ddfdeee9e9acc4
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-09-11 11:52:37 +0900

    [nextjs-ts-practice]refactor: replace react-cookie with js-cookie for cookie setting in useToggleTheme component

--- nextjs-ts-practice/hooks/use-toggle-theme.tsx
+++ nextjs-ts-practice/hooks/use-toggle-theme.tsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import { useCookies } from "react-cookie";
+import Cookies from "js-cookie";
 import { Theme } from "@/types/theme";
 
 export default function useToggleTheme(defaultTheme: Theme = "light") {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
-  const [_, setCookie] = useCookies(["theme"]);
 
   const toggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
-    setCookie("theme", newTheme);
+    Cookies.set("theme", newTheme);
 
     document.documentElement.classList.replace(theme, newTheme);
   };
