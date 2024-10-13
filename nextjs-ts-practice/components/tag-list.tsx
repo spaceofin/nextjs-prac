32cd57e4aae7d14712d89b2790e0533c13b4290a
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-10-13 18:45:47 +0900

    [nextjs-ts-practice]feat: filter posts by clicking on tag in tag list on blog page

--- nextjs-ts-practice/components/tag-list.tsx
+++ nextjs-ts-practice/components/tag-list.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
 export default function TagList({ tags }: { tags: string[] }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const clickedTag = searchParams.get("tags");
+
   return (
     <div className="absolute top-0 -left-3/4 w-3/5 h-full rounded-lg bg-blue-500 bg-opacity-70 px-7 py-10 overflow-y-auto">
       <div className="w-full flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <span className="rounded-lg text-white bg-opacity-70 border-2 border-blue-200 py-1 px-2">
-            {tag}
-          </span>
-        ))}
+        {tags.map((tag) => {
+          return (
+            <Link
+              key={tag}
+              href={`${pathname}?tags=${tag}`}
+              className={`rounded-lg text-white bg-opacity-70 border-2 bg- border-blue-200 py-1 px-2 hover:cursor-pointer active:scale-95 active:bg-blue-600 ${
+                clickedTag === tag ? "bg-blue-600" : "bg-transparent"
+              }`}>
+              {tag}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
