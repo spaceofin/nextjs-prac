6194e9c6e745e709c9ceb12dd410072ad2e3927f
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-10-21 19:53:25 +0900

    [nextjs-ts-practice]refactor: use StyledLink component for navigation Links, remove navigation.module.css, and update styles

--- nextjs-ts-practice/components/navigation.tsx
+++ nextjs-ts-practice/components/navigation.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
-import styles from "./navigation.module.css";
 import ToggleTheme from "./toggle-theme";
 import useServerTheme from "@/hooks/use-server-theme";
 import { Theme } from "@/types/theme";
+
+function StyledLink({ label, href }: { label: string; href: string }) {
+  return (
+    <Link
+      href={href}
+      className="block min-w-24 lg:inline lg:p-2 lg:m-0 text-center hover:bg-slate-700 hover:rounded-lg hover:text-white dark:hover:bg-white dark:hover:bg-opacity-50 dark:hover:text-slate-700">
+      {label}
+    </Link>
+  );
+}
 
 export default function Navigation() {
   const theme: Theme = useServerTheme();
@@ -10,29 +19,19 @@ export default function Navigation() {
     <nav className="font-mono">
       <ul className="relative w-screen min-h-32 m-10 flex flex-col lg:flex-row lg:space-x-4 space-y-1 justify-center items-center text-2xl lg:text-3xl z-0">
         <li>
-          <Link href="/" className={styles.link}>
-            Home
-          </Link>
+          <StyledLink href="/" label="Home" />
         </li>
         <li>
-          <Link href="/blog" className={styles.link}>
-            Blog
-          </Link>
+          <StyledLink href="/blog" label="Blog" />
         </li>
         <li>
-          <Link href="/photos" className={styles.link}>
-            Photos
-          </Link>
+          <StyledLink href="/photos" label="Photos" />
         </li>
         <li>
-          <Link href="/memos" className={styles.link}>
-            Memos
-          </Link>
+          <StyledLink href="/memos" label="Memos" />
         </li>
         <li>
-          <Link href="/todos" className={styles.link}>
-            Todos
-          </Link>
+          <StyledLink href="/todos" label="Todos" />
         </li>
         <li className="flex justify-center pt-1 w-14">
           <ToggleTheme defaultTheme={theme} />
