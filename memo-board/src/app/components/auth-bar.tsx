c4d1c77903fbd25c50100d3af2dccf7aae236439
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-12-07 23:17:40 +0900

    [memo-board]feat: add sign-out button for authenticated users and update prisma schema

--- memo-board/src/app/components/auth-bar.tsx
+++ memo-board/src/app/components/auth-bar.tsx
@@ -2,26 +2,47 @@
 
 import { useSession } from "next-auth/react";
 import { signIn } from "../api/auth/sign-in";
+import { signOut } from "next-auth/react";
 
 export default function AuthBar() {
   const session = useSession();
 
   let authContent: React.ReactNode;
+
   if (session.status === "loading") {
-    authContent = <></>;
+    authContent = (
+      <>
+        <div className="flex justify-end pr-4 gap-2 mb-0">
+          <button className="flex justify-center items-center w-20 h-10 border-2 border-lime-500 bg-lime-100 rounded-md" />
+          <button className="flex justify-center items-center w-20 h-10 bg-lime-400 rounded-md" />
+        </div>
+      </>
+    );
   } else if (session.data?.user) {
-    authContent = <></>;
+    authContent = (
+      <>
+        <div className="flex justify-end pr-4 gap-2 mb-0">
+          <button
+            className="flex justify-center items-center w-24 h-10 border-2 border-lime-500 bg-lime-100 rounded-md"
+            onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </div>
+      </>
+    );
   } else {
     authContent = (
       <>
         <div className="flex justify-end pr-4 gap-2 mb-0">
           <form action={signIn}>
-            <button className="border-2 border-lime-500 bg-lime-100 px-3 py-1 rounded-md">
-              SignIn
+            <button className="flex justify-center items-center w-20 h-10 border-2 border-lime-500 bg-lime-100 rounded-md">
+              Sign In
             </button>
           </form>
           <form>
-            <button className="bg-lime-400 px-3 py-1 rounded-md">SignUp</button>
+            <button className="flex justify-center items-center w-20 h-10 bg-lime-400 rounded-md">
+              Sign Up
+            </button>
           </form>
         </div>
       </>
