06f4c270d4f1e55031b4f597ada1245fa0a6f0dc
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-02-18 14:50:17 +0900

    [memo-board]feat: display dummy group memos card in Group Memos Section for non-logged-in users

--- memo-board/src/app/components/group-memos-section.tsx
+++ memo-board/src/app/components/group-memos-section.tsx
@@ -11,6 +11,40 @@ import {
 } from "@/redux/features/groups/pinnedGroupsSlice";
 import { useSession } from "next-auth/react";
 
+const welcomeGroupMemos = [
+  {
+    title: "Welcome to the Group!",
+    content:
+      "We’re excited to have you join our community. Feel free to introduce yourself and explore our ideas!",
+  },
+  {
+    title: "Day To Do",
+    content: "Today, we’ll be focusing on fixing errors.",
+  },
+];
+
+function DummyGroupMemosCard() {
+  return (
+    <div className="w-full h-full">
+      <div className="flex flex-col h-full gap-2">
+        <h2 className="text-xl px-2">{"Group Memos"}</h2>
+        <div className="flex flex-col flex-grow rounded-md bg-green-50 overflow-y-auto px-4 py-6 gap-1">
+          {welcomeGroupMemos.map((memo) => (
+            <div
+              key={memo.title}
+              className="flex flex-col bg-white border border-gray-700 rounded-md px-2 py-1 text-lg">
+              <p>{memo.title}</p>
+              <p className="text-sm max-h-10 overflow-y-hidden">
+                {memo.content}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function GroupMemosSection() {
   const [isMyGroupVisible, setIsMyGroupVisible] = useState(false);
   const session = useSession();
@@ -40,6 +74,7 @@ export default function GroupMemosSection() {
         {pinnedGroups.map((group) => (
           <GroupMemosCard key={group.id} group={group} />
         ))}
+        {session.status !== "authenticated" && <DummyGroupMemosCard />}
       </div>
     </div>
   );
