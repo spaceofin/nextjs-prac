918779ce0abb0d6441aa9f7800adebc8a2468d6e
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-02-28 11:26:37 +0900

    [nextjs-ts-practice]feat: add generateStaticParams to switch memo detail page from dynamic to static generation

--- nextjs-ts-practice/app/memos/[slug]/page.tsx
+++ nextjs-ts-practice/app/memos/[slug]/page.tsx
@@ -1,4 +1,14 @@
+import fs from "fs";
+import path from "path";
 import { getContent } from "@/lib/load-content";
+
+export async function generateStaticParams() {
+  const memoFiles = fs.readdirSync(path.join(process.cwd(), "content/memos"));
+
+  return memoFiles.map((memo) => ({
+    slug: memo.replace(".mdx", ""),
+  }));
+}
 
 export default async function MemoDetailPage({
   params,
